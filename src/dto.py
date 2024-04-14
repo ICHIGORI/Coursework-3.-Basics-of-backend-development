63578d13d0f1e07a2ad8bc20c09d69ad7b414fb7
Vadim Glinnikov <vadimzepis@gmail.com>
2024-04-15 00:26:42 +0300

    add docstrings

--- src/dto.py
+++ src/dto.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 
 def get_operations_data(name: str = "operations.json", path: str = None) -> list:
+    """Извлекает и возвращает, данные из json файла
+    по умолчанию открывает файл по имени в ~/data директории"""
     path = path if path else os.path.join(Path(__file__).parent.parent, f"data/{name}")
     with open(path, encoding="utf-8") as file:
         return json.load(file)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 def create_operations(operations_data: list[dict]) -> list[Operation]:
+    """Создаёт список объектов Operation из данных json файла"""
     operations_obj = []
     operations = operations_data
     for operation in operations:
@@ -14,6 +15,7 @@ def create_operations(operations_data: list[dict]) -> list[Operation]:
 
 
 def sorted_operations_from_date(operations: list) -> list:
+    """Сортирует объекты по дате"""
     return sorted(operations, key=lambda x: datetime.strftime(x.date, "%Y-%m-%d %H:%M:%S"), reverse=True)
 
 
